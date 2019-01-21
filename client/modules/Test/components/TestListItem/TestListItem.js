8707567b5588c64c0fa8672da112e51510bfac16
Graeme Harvey <graeme@plangrid.com>
2019-01-21 15:50:04 -0500

    Use moment.js to better format datetimes

--- client/modules/Test/components/TestListItem/TestListItem.js
+++ client/modules/Test/components/TestListItem/TestListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
+import moment from 'moment';
 
 // Import Style
 import styles from './TestListItem.css';
@@ -16,7 +17,7 @@ function TestListItem(props) {
       </h3>
       <p className={styles['author-name']}><FormattedMessage id="testType" /> {props.test.type}</p>
       <p className={styles['author-name']}><FormattedMessage id="testStability" /> {props.test.isStable.toString()}</p>
-      <p className={styles['author-name']}><FormattedMessage id="lastUpdated" /> {props.test.lastUpdated}</p>
+      <p className={styles['author-name']}><FormattedMessage id="lastUpdated" /> {moment(props.test.lastUpdated).format('MMMM Do YYYY, h:mm a')}</p>
       <p className={styles['test-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deleteTest" /></a></p>
       <hr className={styles.divider} />
     </div>
@@ -29,7 +30,7 @@ TestListItem.propTypes = {
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     isStable: PropTypes.bool.isRequired,
-    lastUpdated: PropTypes.instanceOf(Date).isRequired,
+    lastUpdated: PropTypes.string.isRequired,
   }).isRequired,
   onDelete: PropTypes.func.isRequired,
 };
