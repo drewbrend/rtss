fa41813483ec45e0894a87afecbfa72fc62df50b
Graeme Harvey <graeme@plangrid.com>
2019-01-29 12:28:33 -0500

    Add pass/fail chart to RunDetailPage

--- client/modules/Run/pages/RunDetailPage/RunDetailPage.js
+++ client/modules/Run/pages/RunDetailPage/RunDetailPage.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import moment from 'moment';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryAxis } from 'victory';
 
 // Import Style
 import styles from '../../components/RunListItem/RunListItem.css';
@@ -14,14 +15,49 @@ import { fetchRun } from '../../RunActions';
 // Import Selectors
 import { getRun } from '../../RunReducer';
 
+// Fake chart data to placehold until Results Reducer is working
+const data = [
+  { count: 4, testResult: 'success' },
+  { count: 2, testResult: 'failure' },
+];
+
 export function RunDetailPage(props) {
   return (
     <div>
       <Helmet title={props.run.job} />
       <div className={`${styles['single-test']} ${styles['test-detail']}`}>
-        <h3 className={styles['test-title']}>{props.run.results}</h3>
+        <h3 className={styles['test-title']}>{props.run.job}</h3>
         <p className={styles['author-name']}><FormattedMessage id="testType" /> {props.run.framework}</p>
         <p className={styles['test-desc']}>{moment(props.run.runDate).format('MMMM Do YYYY, h:mm a')}</p>
+      </div>
+      <div className={`${styles['chart-parent']}`} >
+        <VictoryChart
+          theme={VictoryTheme.material}
+          domainPadding={60}
+          animate={{ duration: 1000, easing: 'poly' }}
+        >
+          <VictoryAxis
+            tickValues={[1, 2]}
+            tickFormat={['Pass', 'Fail']}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(t) => `${Math.round(t)}`}
+          />
+          <VictoryBar
+            data={data}
+            x={(d) => d.testResult}
+            y={(d) => d.count}
+            barRatio={1.5}
+            style={{
+              data: {
+                fill: (d) => {
+                  return d.testResult === 'failure' ? '#FF0000' : '#0000FF';
+                },
+              },
+            }}
+          />
+        </VictoryChart>
       </div>
     </div>
   );
